661124b8f5acc76b805d72c79ea60b171976f77c
Nuno Miguel Sousa Gonçalves <nuno.ms.goncalves@madeira.gov.pt>
2025-10-21 13:45:01 +0100

    save

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ setup_entities(nlp)
 
 # 2) Paths (edit FILE_NAME to switch files)
 FILES_DIR = Path("files")
-FILE_NAME = "IIISerie-005-2003-03-03.md"
+FILE_NAME = "IISerie-165-2025-09-12.md"
 #FILE_NAME = "IISerie-247-2003-12-30Supl9.md"
 
 is_serieIII = "iiiserie" in FILE_NAME.lower()
